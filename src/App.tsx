df9cc564b2f6eaf46e7a79d004e84215ab89d92e
Andre Ferreira <andre.ferreira@atta.com.vc>
2022-05-11 06:58:58 -0300

    rename file

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { CheckboxTree } from './components/CheckboxTree'
 import tree from './data/items.json'
-import { getArrayFromJson } from  './utils/turnJsonIntoArrayOfObjects'
+import { getArrayFromJson } from  './utils/getArrayFromJson'
 
 export const App = () => {
 
